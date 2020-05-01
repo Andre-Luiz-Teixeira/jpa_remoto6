9c1f551722e243aa2a9f96d5c0bc4f65cd0a60fe
André Teixeira <40339686+Andre-Luiz-Teixeira@users.noreply.github.com>
2020-05-01 14:24:22 -0300

    inicial

---
+++ src/model/MdlCategoria.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author andre
+ */
+public class MdlCategoria {
+    private int codigo;
+    private String nome;
+}
---
+++ src/model/MdlPessoa.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author andre
+ */
+public class MdlPessoa {
+    private int codigo;
+    private String nome;
+    private double salario;
+}
